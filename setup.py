1e7548a5b237f18c3bf5918a2254d04125492372
yehzhang <simonzhangintp@gmail.com>
2016-12-07 19:07:58 -0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='rapidtest',
+      version='0.1',
+      author='Simon Zhang',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[])
