ffe98ccb8e513022510435e0ca6293c85cb9a156
yehzhang <simonzhangintp@gmail.com>
2016-12-19 02:12:07 -0800

    Add helper functions

--- rapidtest/__init__.py
+++ rapidtest/__init__.py
@@ -1,5 +1,6 @@
 from .cases import Case
 from .data_structures import TreeNode, ListNode
+from .executors import collect
 from .tests import Test
-from .utils import rec_list, unordered, rec_unordered, memo, randints, randbool, MAX_INT, MIN_INT
-
+from .utils import rec_list, unordered, rec_unordered, memo, randints, randbool, randstr, MAX_INT, \
+    MIN_INT, powerset
--- rapidtest/utils.py
+++ rapidtest/utils.py
@@ -1,10 +1,12 @@
+import string
 import sys
-from itertools import count
-from random import randint, random, sample
+from itertools import count, chain, combinations
+from random import randint, random, sample, choice
+
+sentinel = object()
 
 if sys.version_info.major < 3:
     range = xrange
-
 
 MAX_INT = 2 ** 31 - 1
 MIN_INT = -(2 ** 31)
@@ -27,7 +29,11 @@ def rec_cast(f, x):
         return x
 
 
-unordered = sorted
+def unordered(x):
+    try:
+        return sorted(x)
+    except TypeError:
+        return x
 
 
 def rec_unordered(x):
@@ -39,10 +45,11 @@ def super_len(x):
     if hasattr(x, '__len__'):
         return len(x)
 
-    if hasattr(x, '__iter__'):
+    if is_iterable(x):
+        iter_x = iter(x)
         for i in count():
             try:
-                next(x)
+                next(iter_x)
             except StopIteration:
                 return i
 
@@ -50,11 +57,7 @@ def super_len(x):
 
 
 def is_iterable(x):
-    try:
-        iter(x)
-    except Exception:
-        return False
-    return True
+    return hasattr(x, '__iter__')
 
 
 def memo(f):
@@ -82,28 +85,103 @@ def nop(*args, **kwargs):
 
 
 def randints(count=20, unique=False, max_num=100, min_num=0):
-    """Generate a list of random integers within the range [min_num, max_num]
-
+    """
     :param int count: how many integers are there in the list
     :param bool unique: whether generate no duplicates
     :param int max_num: maximum possible number in the list
     :param int min_num: minimum possible number in the list
-    :return [int]:
+    :return [int]: a `count` length/ list of random integers chosen from the range [min_num,
+        max_num]
     """
     count, min_num, max_num = map(int, (count, min_num, max_num))
-
-    if unique:
-        nums = sample(range(min_num, max_num + 1), count)
-    else:
-        nums = [randint(min_num, max_num) for _ in range(count)]
-
-    return nums
+    return randlist(count, range(min_num, max_num + 1), unique)
 
 
 def randbool(chance=0.5):
     """
     :param float chance: chance of returning True
+    :return bool:
     """
     if not (0 <= chance <= 1):
         raise ValueError('chance is not within the range [0, 1]')
     return random() < chance
+
+
+def randstr(length=None, unique=False, alphabet=string.ascii_lowercase):
+    """
+    :return: a random string
+    """
+    if length is None:
+        length = randint(0, 100)
+    else:
+        length = int(length)
+    return ''.join(randlist(length, alphabet, unique))
+
+
+# noinspection PyIncorrectDocstring
+def randlist(count, alphabet, unique=False):
+    """Generate a list of symbols from alphabet
+
+    :param bool unique: whether there will not be duplicates in returned list
+    :return [type(alphabet), ...]: a `count` length list of random symbols chosen from `alphabet`
+    """
+    if unique:
+        res = sample(alphabet, count)
+    else:
+        res = [choice(alphabet) for _ in range(count)]
+    return res
+
+
+class OneTimeSetProperty(object):
+    """Property that can only be changed once."""
+    cnt_props = 0
+
+    def __init__(self, default=sentinel):
+        self.name = '_{}_{}'.format(type(self).__name__, self.cnt_props)
+        type(self).cnt_props += 1
+
+        self.default = default
+
+    def __get__(self, instance, owner):
+        x = getattr(instance, self.name, self.default)
+        if x is sentinel:
+            raise AttributeError('attribute is not set yet')
+        return x
+
+    def __set__(self, instance, value):
+        if hasattr(instance, self.name):
+            raise AttributeError('cannot reset attribute')
+        setattr(instance, self.name, value)
+
+    def __delete__(self, instance):
+        raise AttributeError('cannot delete attribute')
+
+
+class Sentinel(object):
+    def __init__(self, _str):
+        self.str = _str
+
+    def __str__(self):
+        return self.str
+
+
+def natural_join(last_sep, strs):
+    """Join strs as if in a natural sentence."""
+    strs = list(strs)
+    if not all(isinstance(s, str) for s in strs):
+        raise TypeError('Some of strs is not of type str')
+
+    if not strs:
+        return ''
+    if len(strs) == 1:
+        return strs[0]
+    if len(strs) == 2:
+        l, r = strs
+        return ' '.join([l, last_sep, r])
+    strs[-1] = '{} {}'.format(last_sep, strs[-1])
+    return ', '.join(strs)
+
+def powerset(iterable):
+    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
